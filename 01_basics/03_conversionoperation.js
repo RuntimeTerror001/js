0f0ee266d39d717d7d29bd879155963f7f56a3fc
Aahaan Sethi <aahaansethi1001@gmail.com>
2024-09-19 20:39:45 +0530

    conversion_operation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score = "33abc"
+/* let score = null
+let score = undefined */
+
+/*console.log(typeof score);
+console.log(typeof (score)); */
+
+let valueinnumber = Number(score)
+/* console.log(typeof valueinnumber);
+console.log(valueinnumber); */
+
+
+// "33" => 33
+// "33abc" =>  NaN   (Not an Number)
+// true => 1 ; false => 0
+
+let isLoggedin = 1
+
+let booleanisLoggedin = Boolean(isLoggedin)
+console.log(booleanisLoggedin);
+
+
+// 1=> true ; 0=> false
+// "" => false
+// "Aahaan" => true
+
+let somenumber = 33
+let stringnumber = String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber );
+
