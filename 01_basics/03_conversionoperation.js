e6ecd69aeadf100ae4b3ce376cddc095c99e260f
Aahaan Sethi <aahaansethi1001@gmail.com>
2024-09-21 01:03:48 +0530

    conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -26,6 +26,37 @@ console.log(booleanisLoggedin);
 
 let somenumber = 33
 let stringnumber = String(somenumber);
-console.log(stringnumber);
-console.log(typeof stringnumber );
+//console.log(stringnumber);
+//console.log(typeof stringnumber );
 
+// *****************Operations***************
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+
+//console.log(2+2);
+//console.log(2**3);
+
+let str1 = "hello"
+let str2 = " Aahaan"
+
+let str3 = str1+ str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
