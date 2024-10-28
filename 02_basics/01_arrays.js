906b21706393c71340874fa1d36416e7d27fd58e
Aahaan Sethi <aahaansethi1001@gmail.com>
2024-10-28 23:50:18 +0530

    array basics 2.O

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -31,17 +31,17 @@ const newarr = myarr.join()
 
 // slice , splice
 
-console.log("A ", myarr);
+//console.log("A ", myarr);
 
 const myn1 = myarr.slice(1,3)
 
-console.log(myn1);
-console.log("B ", myarr);
+//console.log(myn1);
+//console.log("B ", myarr);
 
 const myn2 = myarr.splice(1,3)
 
-console.log("C ", myarr);
-console.log(myn2);
+//console.log("C ", myarr);
+//console.log(myn2);
 
 
 
